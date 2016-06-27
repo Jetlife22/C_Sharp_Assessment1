5707233c8c64212699e79e699b2ec77d9af6146b
Jetlife22 <a.alhaj@hotmail.co.uk>
2016-06-27 08:26:11 +0100

    progress

---
+++ C_sharpAssessment/C_sharpAssessment/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace C_sharpAssessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(12, 266);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(100, 23);
+            this.displayButton.TabIndex = 0;
+            this.displayButton.Text = "Display books";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(283, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(283, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(283, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(283, 212);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(134, 266);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(100, 23);
+            this.nextButton.TabIndex = 5;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(269, 266);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(75, 23);
+            this.previousButton.TabIndex = 6;
+            this.previousButton.Text = "Previous";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(386, 266);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 7;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ISBN Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Price";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(134, 307);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 12;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(269, 307);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 13;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 348);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.displayButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ C_sharpAssessment/C_sharpAssessment/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_sharpAssessment
+{
+    public partial class Form1 : Form
+    {
+        public static int numberOfBooks = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            bookList.Add(book1);
+            bookList.Add(book2);
+            bookList.Add(book3);
+            bookList.Add(book4);
+            bookList.Add(book5);
+
+        }
+
+
+        //declared the list as a public static, so it can be used in the class without the need to be instantiated via another method below
+        public static List<Book> bookList = new List<Book>();
+
+        Book book1 = new Book("title1", "author1", "ISBN1", 1);
+        Book book2 = new Book("title2", "author2", "ISBN2", 2);
+        Book book3 = new Book("title3", "author3", "ISBN3", 3);
+        Book book4 = new Book("title4", "author4", "ISBN4", 4);
+        Book book5 = new Book("title5", "author5", "ISBN5", 5);
+
+
+        //the DISPLAY button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(Book b in bookList)
+            {
+                textBox1.Text = bookList[0].Title;
+                textBox2.Text = bookList[0].Author;
+                textBox3.Text = bookList[0].ISBN;
+                textBox4.Text = (bookList[0].Price).ToString();
+                BackColor = Color.Azure;
+            }
+        }
+
+        //the NEXT button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //if the index nuber assigned is smaller than the booklist count,
+            if (numberOfBooks < bookList.Count - 1)
+            {
+                //increment number of books index by 1, and asign number to the array position of the booklist items
+                    numberOfBooks += 1;
+                    textBox1.Text = bookList[numberOfBooks].Title;
+                    textBox2.Text = bookList[numberOfBooks].Author;
+                    textBox3.Text = bookList[numberOfBooks].ISBN;
+                    textBox4.Text = (bookList[numberOfBooks].Price).ToString();
+            }
+        }
+        
+        //the PREVIOUS button
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            //same statement as above for the next button, but with a decrement
+            if (numberOfBooks > 0)
+            {
+                numberOfBooks -= 1;
+                textBox1.Text = bookList[numberOfBooks].Title;
+                textBox2.Text = bookList[numberOfBooks].Author;
+                textBox3.Text = bookList[numberOfBooks].ISBN;
+                textBox4.Text = (bookList[numberOfBooks].Price).ToString();
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+
+
+    //created a public class Book, available to be accessed by other classes wthin the project
+    public class Book
+    {
+
+        ////declared private string variables/fields
+        public string title;
+        public string author;
+        public string isbn;
+        public int price;
+
+
+
+        public Book(string Title, string Author, string ISBN, int Price)
+        {
+            title = Title;
+            author = Author;
+            isbn = ISBN;
+            price = Price;
+
+            
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string ISBN
+        {
+            get
+            {
+                return isbn;
+            }
+            set
+            {
+                isbn = value;
+            }
+
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+
+        }
+
+
+    }
+
+}
